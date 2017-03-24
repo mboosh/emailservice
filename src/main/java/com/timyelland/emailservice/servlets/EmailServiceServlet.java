06319de680817d8da7ba80b8f5b3b8e9d8900f3d
Tim Yelland <timyelland@gmail.com>
2017-03-24 08:08:52 +0200

    Initial Commit

---
+++ src/main/java/com/timyelland/emailservice/servlets/EmailServiceServlet.java
@@ -0,0 +1,9 @@
+package com.timyelland.emailservice.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/emailservice")
+public class EmailServiceServlet extends HttpServlet {
+
+}
