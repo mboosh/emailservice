80e61ebd6b7c3859f82207d7becdf5c244ca9d9b
Tim Yelland <timyelland@gmail.com>
2017-03-29 00:34:17 +0200

    Minor Update

--- src/test/java/com/timyelland/emailservice/handler/EmailHandlerTest.java
+++ src/test/java/com/timyelland/emailservice/handler/EmailHandlerTest.java
@@ -6,10 +6,14 @@ import com.timyelland.emailservice.handler.impl.EmailHandlerImpl;
 
 public class EmailHandlerTest {	
 	@Test
-	public void testSomething() {
+	public void testPositiveEmailHandlerChain() {
+		
 		final EmailHandler handler1 = new EmailHandlerImpl(null);
 		final EmailHandler handler2 = new EmailHandlerImpl(null);
 		final EmailHandler handler3 = new EmailHandlerImpl(null);
+		
+		handler1.nextHandler(handler2, new TestSmtpHandlerImpl());
+		handler1.nextHandler(handler3, new TestSmtpHandlerImpl());
 		
 		handler1.handleRequest(null);
 	}
