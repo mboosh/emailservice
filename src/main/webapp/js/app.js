fa168c6affe89b608a8acf5c482c6bdaa6c08f35
Tim Yelland <timyelland@gmail.com>
2017-04-03 08:41:12 +0200

    Merge

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-/**
- * 
- */
-var emailServiceApp = angular.module('EmailServiceApp', ["ngMessages"]);
-
-var emailServiceController = emailServiceApp.controller('EmailServiceController', ['$scope', '$http', function($scope, $http) {  
-  $scope.submitForm = function() {
-	$scope.delay = true;
-	  
-    emailData = {
-	  toEmail: $scope.toemail,
-	  ccEmail: $scope.ccemail,
-	  subject: $scope.subject,
-	  content: $scope.content
-    };	  
-	  
-	var res = $http.post('service', emailData);
-	res.success(function(data, status, headers, config) {
-		$scope.delay = false;
-		$scope.message = data;
-	});
-	res.error(function(data, status, headers, config) {
-		$scope.delay = false;
-		alert("An Error Occured: " + JSON.stringify({data: data}));
-	});
-  };  
-=======
 /**
  * Angular.js Instiantiate EmailServiceApp with corresponding EmailServiceController
  */
@@ -53,5 +25,4 @@ var emailServiceController = emailServiceApp.controller('EmailServiceController'
 		alert("An Error Occured: " + JSON.stringify({data: data}));
 	});
   };  
->>>>>>> branch 'master' of https://github.com/mboosh/emailservice
 }]);
